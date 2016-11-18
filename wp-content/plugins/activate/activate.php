e1f48fd2ce7e678ad81c488cd261796b1d839bad
Arjan de Ruijter <rra@mboutrecht.nl>
2016-11-18 11:17:33 +0100

    Dit is de nieuwe activatiepagina

--- wp-content/plugins/activate/activate.php
+++ wp-content/plugins/activate/activate.php
@@ -7,63 +7,40 @@
       Description: Activatieplugin werkt samen met registrationform 
    */
 
+function activate()
+{
    if ( isset($_POST["submit"]) )
-   {
-         // Maak contact met mysql-server en database
-         include("./connect_db.php");
-         // Maak een query die het record behorende bij het meegegeven id selecteerd.
-         $sql = "SELECT * FROM `users` WHERE `id` = ".$_POST["id"];
-         // Vuur de query af op de database
-         $result = mysqli_query($conn, $sql);
-         // Zet de resource om naar een associatief array
-         $record = mysqli_fetch_array($result, MYSQLI_ASSOC);
-         // Wanneer de via de url meegegeven id - pw combinatie gelijk is aan die in de database...
-         if (!strcmp($_POST["pw"], $record["password"]))
-         {
-             
-               
-                  if (strcmp($_POST["password"], $_POST["verification_password"]) == 0)
-                  {
-                        // dan updaten we het password veld naar de nieuwe waarde en zetten we activate op true.
-                        $sql = "UPDATE `users` SET `password` = '".sha1($_POST["password"])."',
-                                                      `activate` = 'true'
-                              WHERE              `id` = ".$_POST["id"].";";
-                        $result = mysqli_query($conn, $sql);
-                        if ($result)
-                        {
-                              echo "Uw account is geactiveerd en uw password gewijzigd.";
-                              header("refresh: 4; url=index.php?content=login_form&email=".$record["email"]);
-                        }
-                  }
-                  else
-                  {
-                        echo "De twee ingevoerde wachtwoorden komen niet overeen. Probeer het nog een keer.<br>";
-                        echo "U wordt doorgestuurd naar de betreffende pagina";
-                        header("refresh: 4; url=index.php?content=activate&id=".$_POST["id"]."&pw=".$_POST["pw"]);
-                  }
-              
-         }
-         else
-         {
-               // Wanneer de via de url meegegeven id - pw combinatie niet gelijk is aan die in de database...
-               // Dan melden we de onderstaande tekst en sturen we door naar de homepage.
-               if ($record["activate"] )
-               {
-                     echo "Uw account is al geactiveerd. U wordt nu doorgestuurd naar de inlogpagina.";
-                     header("refresh: 4; url=index.php?content=login_form&email=".$record["email"]);
-               }
-               else
-               {
-                  echo "U heeft geen rechten op deze pagina, u wordt doorgestuurd naar de homepage";
-                  header("refresh: 400; url=index.php?content=home");
-               }
-         }
-   }
+   {         
+       // Vraag de gegevens op van de user uit de database
+       $user_info = get_userdata( $_POST["id"] );
+
+       //var_dump($user_info); 
+                      
+       if (strcmp($_POST["password"], $_POST["verification_password"]) == 0)
+       {
+            // dan updaten we het password veld naar de nieuwe waarde en zetten we activate op true.
+            $user_id = wp_set_password( $_POST["password"], $_POST["id"] );
+
+            //var_dump($user_id); exit();
+            if ( ! is_wp_error( $user_id ) )
+            { 
+                  echo "Uw account is geactiveerd en uw password gewijzigd.";
+                  header("refresh: 4; url=http://localhost/2016-2017/am1b/groenten/");
+            }                  
+            
+        }
+        else
+        {
+            echo "De twee ingevoerde wachtwoorden komen niet overeen. Probeer het nog een keer.<br>";
+            echo "U wordt doorgestuurd naar de betreffende pagina";
+            header("refresh: 4; url=http://localhost/2016-2017/am1b/groenten/index.php/activatie/?content=activate&id=".$_POST["id"]."&pw=".MD5("geheim")."'");
+        }
+     }
    else
    {
 ?>
 <h2>Wijzig uw wachtwoord</h2>
-<form action="./index.php?content=activate" method="post">
+<form action="<?php echo esc_url($_SERVER['PHP_SELF']); ?>" method="post">
    <table>
       <tr>
          <td>Wachtwoord:</td>
@@ -83,4 +60,6 @@
 </form>
 <?php
    }
+}
+add_shortcode("activate", "activate");
 ?>
